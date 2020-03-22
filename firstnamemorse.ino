8f340e1fc8a868aef1f3281e705e5efa2f209af6
chris-yl31 <62497221+chris-yl31@users.noreply.github.com>
2020-03-22 18:46:46 +1100

    Add files via upload

---
+++ firstnamemorse.ino
@@ -0,0 +1,216 @@
+// ------------
+// MORSE CODE
+// ------------
+
+int led1 = D6;
+
+int led2 = D7;
+
+void setup() {
+
+	pinMode(led1, OUTPUT);
+	pinMode(led2, OUTPUT);
+
+}
+
+void loop() {
+	
+    // LETTER C
+    
+    
+    // LONG
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(500);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// LONG
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(500);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(750);
+	
+	// LETTER H
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(750);
+	
+	// LETTER R
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// LONG
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(500);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(750);
+	
+	// LETTER I
+
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(750);
+	
+	// LETTER S
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(350);
+	
+	// SHORT
+	
+	digitalWrite(led1, HIGH);
+	digitalWrite(led2, HIGH);
+
+	delay(200);
+
+	digitalWrite(led1, LOW);
+	digitalWrite(led2, LOW);
+	
+	delay(750);
+}
